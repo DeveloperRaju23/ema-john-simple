bcb397bb5059490e8fa60a642c781a4e2a1437e8
Developerraju23 <rajukumaraju.rk@gmail.com>
2023-03-27 20:51:55 +0600

    calculation cart details

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = (props) => {
+    const {cart} = props;
+
+    // Total Price 
+     let totalPrice = 0;
+     let totalShipping = 0;
+     for(const product of cart){
+        totalPrice = totalPrice + product.price
+        totalShipping = totalShipping + product.shipping;
+     }
+
+     const tax = totalPrice* 7 / 100;
+     const grandTotal = totalPrice + totalShipping + tax 
+
+    return (
+        <div className='cart'>
+            <h4 className='order-summary'>Order Summary</h4>
+             <h4 className='items'>Selected Items : {cart.length}</h4>
+             <h4>Total Price : $ {totalPrice} </h4>
+             <h4>Total Shipping Charge : $ {totalShipping}  </h4>
+             <h4>Tax : $ {tax} </h4>
+             <h2>Grand Total  : $ {grandTotal} </h2>
+        </div>
+    );
+};
+
+export default Cart;
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import './Product.css'
+
 const Product = (props) => {
-    console.log(props.product)
-    const {img , name , price , seller , ratings} = props.product
+    const {img , name , price , seller , ratings} = props.product;
+
+const handleAddToCart = props.handleAddToCart;
+
     return (
         <div className='product'>
-            <img src={img} alt="" />
+            <img src={img}  alt="" />
             <div className='product-info'>
                     <h5 className='product-name'>{name}</h5>
                     <p>${price}</p>
                     <p>Manufacturer : {seller}</p>
-                    <p>Rating : {ratings}</p>
+                    <p>Rating : {ratings} Stars</p>
             </div>
-            <button className='btn-cart'>Add to Cart</button>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>Add to Cart <i class="fa-solid fa-cart-shopping"></i>  </button>
         </div>
     );
 };
