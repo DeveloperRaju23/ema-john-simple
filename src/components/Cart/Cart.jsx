ea749d1b6de11eccfa79c631c63968cd688b3b96
Developerraju23 <rajukumaraju.rk@gmail.com>
2023-03-27 21:48:25 +0600

    add too local store setup

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,20 +5,20 @@ const Cart = (props) => {
     const {cart} = props;
 
     // Total Price 
-     let totalPrice = 0;
+     let totalPrice = 0; 
      let totalShipping = 0;
      for(const product of cart){
         totalPrice = totalPrice + product.price
         totalShipping = totalShipping + product.shipping;
      }
 
-     const tax = totalPrice* 7 / 100;
+     const tax = totalPrice * 7 / 100;
      const grandTotal = totalPrice + totalShipping + tax 
 
     return (
         <div className='cart'>
             <h4 className='order-summary'>Order Summary</h4>
-             <h4 className='items'>Selected Items : {cart.length}</h4>
+             <h4 className='items'>Selected Items : {cart.quantity}</h4>
              <h4>Total Price : $ {totalPrice} </h4>
              <h4>Total Shipping Charge : $ {totalShipping}  </h4>
              <h4>Tax : $ {tax} </h4>
